789fdb138bf5f8ed9310e6e69c6002f4a587e9e5
HoshinoTented <limbolrain@gmail.com>
2019-02-20 22:33:24 +0800

    难诶

--- setup.ubuntu.16.04.sh
+++ setup.ubuntu.16.04.sh
@@ -2,86 +2,93 @@
 
 work_dir="~/Documents/Projects/Unknown"
 
-mkdir -p ${work_dir}
-mkdir -p ~/.local/bin/
-mkdir -p ~/.emacs.d/
+function sayHello() {
+    echo "QWQ"
+}
 
-cd ${work_dir}
+function predo() {
+    mkdir -p ${work_dir}
+    mkdir -p ~/.local/bin/
+    mkdir -p ~/.emacs.d/
+}
 
-cp ./init.el ~/.emacs.d/
+function emacsScript() {
+    cp ./init.el ~/.emacs.d/
+}
 
 # repositories
 # sudo add-apt-repository ppa:noobslab/themes
 
-sudo apt update
-
-# common tools
-sudo apt install git curl emacs24 zip
+function apts() {
+    sudo apt update
+    sudo apt install git curl emacs24 zip
+}
 
 # flatabulous unity-tweak-tool
 # sudo apt install flatabulous-theme unity-tweak-tool
 
-# jdks
-sudo apt install openjdk-8-jdk openjdk-8-source openjfx openjfx-source
+function jdks() {
+    sudo apt install openjdk-8-jdk openjdk-8-source openjfx openjfx-source
+}
 
-# repository
-echo "Cloning repository"
-git clone https://github.com/HoshinoTented/TrashBin.git
 
-cd TrashBin
+function cloneRepo() {
+    echo "Cloning repository"
+    git clone https://github.com/HoshinoTented/TrashBin.git
+    # cd TrashBin
+}
 
-# hoshino's terminal configurations
-init_sh=".hoshino_init.sh"
-echo "Configuring ${init_sh}"
+function initShell() {
+    init_sh=".hoshino_init.sh"
+    echo "Configuring ${init_sh}"
 
-ln ${init_sh} ~/
-echo -e "\nsource ${init_sh}" >> ~/.bashrc
+    ln ${init_sh} ~/
+    echo -e "\nsource ${init_sh}" >> ~/.bashrc
 
-source ~/.bashrc
+    source ~/.bashrc
+}
 
-# ssr
-echo "Configuring ShadowsockR"
+function ssr() {
+    echo "Configuring ShadowsockR"
 
-ssr_install="~/.local/bin/ssr"
-# curl -sSL https://github.com/the0demiurge/CharlesScripts/blob/master/charles/bin/ssr?raw=true > ${ssr_install}
-# chmod +x ${ssr_install}
-ln ./ssr ${ssr_install}
-ssr install
+    ssr_install="~/.local/bin/ssr"
+    # curl -sSL https://github.com/the0demiurge/CharlesScripts/blob/master/charles/bin/ssr?raw=true > ${ssr_install}
+    # chmod +x ${ssr_install}
+    ln ./ssr ${ssr_install}
+    ssr install
 
-echo "Please configure your ssr"
-ssr config
-ssr start
+    echo "Please configure your ssr"
+    ssr config
+    ssr start
+}
 
-# haskell-stack
-echo "Configuring Haskell-Stack"
+function haskellstack() {
+    echo "Configuring Haskell-Stack"
 
-stack_config="~/.stack"
-mkdir ${stack_config}
-curl -sSL https://get.haskellstack.org/ | sh
-cp ./haskellstack/config.yaml ${stack_config}/config.yaml
-stack setup #--resolver=ghc-8.6.3
+    stack_config="~/.stack"
+    mkdir ${stack_config}
+    curl -sSL https://get.haskellstack.org/ | sh
+    cp ./haskellstack/config.yaml ${stack_config}/config.yaml
+    stack setup #--resolver=ghc-8.6.3
 
-## haskell packages
-stack install vector
+    ## haskell packages
+    stack install vector
+}
 
-# sdkman | install kotlin, gradle
-echo "Configuring SDKMAN!"
-useProxy
+function sdkman() {
+    echo "Configuring SDKMAN!"
 
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+    curl -s "https://get.sdkman.io" | bash
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-sdk install kotlin
-sdk install gradle
+    sdk install kotlin
+    sdk install gradle
+}
 
-removeProxy
+function chrome() {
+    echo "Installing Chrome"
 
-# Chrome
-echo "Installing Chrome"
-useProxy
-
-chrome_install="/tmp/chrome.deb"
-wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O ${chrome_install}
-sudo dpkg -i ${chrome_install}
-
-removeProxy
+    chrome_install="/tmp/chrome.deb"
+    wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O ${chrome_install}
+    sudo dpkg -i ${chrome_install}
+}
